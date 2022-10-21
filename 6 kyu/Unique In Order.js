af10f18e3caf23761b732421670b02f09a1bcf6e
Muhammad <105668626+SomeMuhammad@users.noreply.github.com>
2022-10-21 10:57:52 +0400

    Create Unique In Order.js

---
+++ 6 kyu/Unique In Order.js
@@ -0,0 +1,5 @@
+function uniqueInOrder (iterable) {
+        let arr = [];
+        for (let i=0;i<iterable.length;i++) if (iterable[i] !== iterable[i+1]) arr.push(iterable[i]);
+        return arr;
+}
